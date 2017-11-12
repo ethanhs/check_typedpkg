3fbc7935feb8bbd87d81b27122ca552709115382
Ethan Smith <ethan@ethanhs.me>
2017-11-12 03:22:43 -0800

    Update for latest version of PEP

--- typed_check/__main__.py
+++ typed_check/__main__.py
@@ -4,48 +4,38 @@ import os
 import sys
 import re
 import os.path
+from distutils.sysconfig import get_python_lib
 from typing import Optional
 
-TYPED_PATTERN = r'Typed: (.*)\n'
-
-def check_metadata(path: str) -> Optional[str]:
-    """Returns the static typing metadata information. This is either
-    "inline", "stubs", or None
-    """
-    with open(path) as f:
-        for line in reversed(f.readlines()):
-            info = re.search(TYPED_PATTERN, line)
-            if info:
-                return info.group(1)
+def istyped(directory: str) -> bool:
+    return os.path.exists(os.path.join(directory, 'py.typed'))
 
 def find_info(pkg_name: str) -> str:
     """
-    Gets path to metadata file installed in either the OS sitepackages or the
-    user sitepackages. It returns the path to either *-*.dist-info/METADATA or
-    *-*.egg-info
+    Checks site-/dist-packages for a package of given name, and reports if it
+    is typed or not.
     """
-    paths = []
-    for dir in site.getsitepackages() + [site.getusersitepackages()]:
-        paths.extend(glob.glob(os.sep.join([dir, f'{pkg_name}-*.*-info'])))
-    assert len(paths) > 0, f"Did not find metadata for package {pkg_name}"
-    assert len(paths) == 1, f"Conflicting packages found: {paths}"
-        
-    path = paths[0]
-    typed: Optional[str] = None
-    if os.path.isfile(path) and path.endswith(".egg-info"):
-        # .egg-info file, just read it
-        typed = check_metadata(path)
-    elif os.path.isdir(path) and path.endswith(".dist-info"):
-        # .dist-info folder, read the METADATA file
-        meta_file = os.sep.join([path, "METADATA"])
-        assert os.path.isfile(meta_file), f"Could not find a METADATA file in {path}"
-        typed = check_metadata(meta_file)
-    if typed == 'inline':
-        print(f'Package {pkg_name} has inline type annotations')
-    elif typed == 'stubs':
-        print(f'Package {pkg_name} has stub files')
-    elif typed is None:
-        print(f'Package {pkg_name} is not typed')
+    try:
+        pkg_dir = site.getusersitepackages()
+        pkg_dirs = site.getsitepackages() + [pkg_dir]
+    except AttributeError:
+        pkg_dirs = [get_python_lib()]
+    for directory in pkg_dirs:
+        pkg = os.path.join(directory, pkg_name)
+        if os.path.exists(pkg):
+            if istyped(pkg):
+                print("Package {} has packaged type information.".format(pkg_name))
+            else:
+                print("Package {} does not support typing.".format(pkg_name))
+        elif os.path.exists(pkg + '_stubs'):
+            if istyped(pkg+'_stubs'):
+                print("Package {} is a stub only package.".format(pkg_name))
+            else:
+                print("Package {} does not support typing.".format(pkg_name))
+        else:
+            continue
+
+    
 
 
 def main():
