118a38b92190abca672049b6703e38a9227d355e
Ethan Smith <ethan@ethanhs.me>
2017-11-15 14:24:18 -0800

    Report if tool cannot find package, style changes

--- typed_check/__main__.py
+++ typed_check/__main__.py
@@ -1,16 +1,15 @@
 import site
-import glob
 import os
 import sys
-import re
 import os.path
 from distutils.sysconfig import get_python_lib
-from typing import Optional
+
 
 def istyped(directory: str) -> bool:
     return os.path.exists(os.path.join(directory, 'py.typed'))
 
-def find_info(pkg_name: str) -> str:
+
+def find_info(pkg_name: str) -> None:
     """
     Checks site-/dist-packages for a package of given name, and reports if it
     is typed or not.
@@ -25,22 +24,26 @@ def find_info(pkg_name: str) -> str:
         if os.path.exists(pkg):
             if istyped(pkg):
                 print("Package {} has packaged type information.".format(pkg_name))
+                return
             else:
                 print("Package {} does not support typing.".format(pkg_name))
+                return
         elif os.path.exists(pkg + '_stubs'):
             if istyped(pkg+'_stubs'):
                 print("Package {} is a stub only package.".format(pkg_name))
+                return
             else:
                 print("Package {} does not support typing.".format(pkg_name))
-        else:
-            continue
-
-    
+                return
+    print("Could not find package {}.".format(pkg_name))
 
 
 def main():
     if sys.argv[1] == '--help':
         print('Usage: python -m typed_check <pkg>')
+        sys.exit(0)
     find_info(sys.argv[1])
+
+
 if __name__ == '__main__':
     main()
