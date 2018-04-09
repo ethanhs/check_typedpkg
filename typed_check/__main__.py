52514eedfbae04b079a557b715dace2f636da838
Ethan Smith <ethan@ethanhs.me>
2018-04-09 16:40:36 -0700

    Update implementation to conform to PEP 561

--- typed_check/__main__.py
+++ typed_check/__main__.py
@@ -18,29 +18,34 @@ def find_info(pkg_name: str) -> None:
         pkg_dir = site.getusersitepackages()
         pkg_dirs = site.getsitepackages() + [pkg_dir]
     except AttributeError:
+        print('Cannot use site module, falling back to sysconfig.')
         pkg_dirs = [get_python_lib()]
-    for directory in pkg_dirs:
-        pkg = os.path.join(directory, pkg_name)
-        if os.path.exists(pkg):
-            if istyped(pkg):
-                print("Package {} has packaged type information.".format(pkg_name))
-                return
-            else:
-                print("Package {} does not support typing.".format(pkg_name))
-                return
-        elif os.path.exists(pkg + '_stubs'):
-            if istyped(pkg+'_stubs'):
-                print("Package {} is a stub only package.".format(pkg_name))
-                return
-            else:
-                print("Package {} does not support typing.".format(pkg_name))
-                return
-    print("Could not find package {}.".format(pkg_name))
-
+    inline = False
+    stub = False
+    exists = False
+    
+    for pkg_dir in pkg_dirs:
+        stub_name = pkg_name + '-stubs'
+        typed_file = os.path.join(pkg_dir, pkg_name, 'py.typed')
+        stub_dir = os.path.join(pkg_dir, stub_name)
+        if os.path.isdir(os.path.join(pkg_dir, pkg_name)):
+            exists = True
+        if os.path.isdir(stub_dir):
+            stub = True
+        elif os.path.isfile(typed_file):
+            inline = True
+    if stub:
+        print("Package {} is a stub only package.".format(pkg_name))
+    elif inline:
+        print("Package {} has inline type information.".format(pkg_name))
+    elif exists:
+        print("Package {} does not support typing.".format(pkg_name))
+    else:
+        print("Could not find package '{}'.".format(pkg_name))
 
 def main():
     if sys.argv[1] == '--help':
-        print('Usage: python -m typed_check <pkg>')
+        print('Usage: python -m typed_check <package name>')
         sys.exit(0)
     find_info(sys.argv[1])
 
